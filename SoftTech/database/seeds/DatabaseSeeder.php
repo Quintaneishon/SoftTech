9362d5e5da1c258afd5ed6d9c1415a22e6c514a7
Quintaneishon <ajitzi.quintana@gmail.com>
2019-03-19 16:45:03 -0600

    Database Seeder

--- SoftTech/database/seeds/DatabaseSeeder.php
+++ SoftTech/database/seeds/DatabaseSeeder.php
@@ -26,7 +26,7 @@ class DatabaseSeeder extends Seeder
         foreach ($tables as $table){
             DB::table($table)->truncate();
         }
-
+        
         DB::statement('SET FOREIGN_KEY_CHECKS = 1;');
     }
 }
